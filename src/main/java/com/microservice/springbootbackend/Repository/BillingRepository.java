892be4b02950cf33370a4a6d936cd6e037e5e063
MrazTevin <millatevin@gmail.com>
2022-10-06 08:31:27 +0300

    Revert "m: created table schema"
    This reverts commit a1902dc4d650eed53850e80713734b99a89b0a2b.

--- src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
+++ src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
@@ -1,11 +1,2 @@
-package com.microservice.springbootbackend.Repository;
-
-import com.microservice.springbootbackend.Models.Billings;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billings, Long> {
-    // Jpa repository enables us perform crud database operations
-
+package com.microservice.springbootbackend.Repository;public interface BillingRepository {
 }
