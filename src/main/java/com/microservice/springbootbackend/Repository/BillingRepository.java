a1902dc4d650eed53850e80713734b99a89b0a2b
MrazTevin <millatevin@gmail.com>
2022-10-06 08:22:37 +0300

    m: created table schema

--- src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
+++ src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
@@ -1,2 +1,11 @@
-package com.microservice.springbootbackend.Repository;public interface BillingRepository {
+package com.microservice.springbootbackend.Repository;
+
+import com.microservice.springbootbackend.Models.Billings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillingRepository extends JpaRepository<Billings, Long> {
+    // Jpa repository enables us perform crud database operations
+
 }
