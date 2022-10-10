e87650efdc4f86070b1435ce34edf06f17927a8d
MrazTevin <millatevin@gmail.com>
2022-10-10 07:55:13 +0300

    m: added a list method

--- src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
+++ src/main/java/com/microservice/springbootbackend/Repository/BillingRepository.java
@@ -2,10 +2,13 @@ package com.microservice.springbootbackend.Repository;
 
 import com.microservice.springbootbackend.Models.Billings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BillingRepository extends JpaRepository<Billings, Long> {
-    // Jpa repository enables us perform crud database operations
-
+    List<Billings> findByName(String name);
+//    List<Billings> findByTitle(String title);
 }
