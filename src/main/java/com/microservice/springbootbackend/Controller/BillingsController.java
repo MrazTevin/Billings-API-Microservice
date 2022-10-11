1bcac910948a84121491f2694a00c53a97e58b1c
MrazTevin <millatevin@gmail.com>
2022-10-11 13:41:47 +0300

    fix: fix error

--- src/main/java/com/microservice/springbootbackend/Controller/BillingsController.java
+++ src/main/java/com/microservice/springbootbackend/Controller/BillingsController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/billings")
-public class git BillingsController {
+public class BillingsController {
     @Autowired
     private BillingRepository billingRepository;
 
