20f5c7af4b029fad290afe62c41e252db542c44d
MrazTevin <millatevin@gmail.com>
2022-10-20 18:59:20 +0300

    m: modelled the user table

--- src/main/java/com/microservice/springbootbackend/Models/User.java
+++ src/main/java/com/microservice/springbootbackend/Models/User.java
@@ -1,2 +1,41 @@
-package com.microservice.springbootbackend.Models;public class User {
+package com.microservice.springbootbackend.Models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "users_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private Long age;
+
+    @Column(name="created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name="updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+
 }
