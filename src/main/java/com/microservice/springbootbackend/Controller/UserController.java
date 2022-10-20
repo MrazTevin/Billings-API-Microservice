ba8fef2999898155ff9c8743a7d7ccfccf0e8ef3
MrazTevin <millatevin@gmail.com>
2022-10-20 18:58:47 +0300

    m: add user endpoints

--- src/main/java/com/microservice/springbootbackend/Controller/UserController.java
+++ src/main/java/com/microservice/springbootbackend/Controller/UserController.java
@@ -1,2 +1,21 @@
-package com.microservice.springbootbackend.Controller;public class UserController {
+package com.microservice.springbootbackend.Controller;
+
+import com.microservice.springbootbackend.Models.User;
+import com.microservice.springbootbackend.Models.UserModel;
+import com.microservice.springbootbackend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> save(@RequestBody UserModel user) {
+        return new ResponseEntity<User>(userService.createUser(user), HttpStatus.CREATED);
+    }
 }
