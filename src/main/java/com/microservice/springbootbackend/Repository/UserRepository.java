32efeb240e2566fc058947da2631b9f3894d681a
MrazTevin <millatevin@gmail.com>
2022-10-20 19:07:36 +0300

    m: enabled crud operations on the user

--- src/main/java/com/microservice/springbootbackend/Repository/UserRepository.java
+++ src/main/java/com/microservice/springbootbackend/Repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.microservice.springbootbackend.Repository;public interface UserRepository {
+package com.microservice.springbootbackend.Repository;
+
+import com.microservice.springbootbackend.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
