14e5d97d4c78a5773e6de4279277cf4d513c0eaa
MrazTevin <millatevin@gmail.com>
2022-10-20 19:08:56 +0300

    m: save the user to the database

--- src/main/java/com/microservice/springbootbackend/Service/UserServiceImpl.java
+++ src/main/java/com/microservice/springbootbackend/Service/UserServiceImpl.java
@@ -1,2 +1,20 @@
-package com.microservice.springbootbackend.Service;public class UserServiceImpl {
+package com.microservice.springbootbackend.Service;
+
+import com.microservice.springbootbackend.Models.User;
+import com.microservice.springbootbackend.Models.UserModel;
+import com.microservice.springbootbackend.Repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User createUser(UserModel user) {
+        User newUser = new User();
+        BeanUtils.copyProperties(user, newUser);
+        return userRepository.save(newUser);
+    }
 }
