191edf1a78a5ceaaeba54566b196765a08278ae0
MrazTevin <millatevin@gmail.com>
2022-10-22 09:18:18 +0300

    m: add validations to the column fields

--- src/main/java/com/microservice/springbootbackend/Models/UserModel.java
+++ src/main/java/com/microservice/springbootbackend/Models/UserModel.java
@@ -2,12 +2,20 @@ package com.microservice.springbootbackend.Models;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserModel {
+    @NotBlank(message = "Your name is required")
     private String name;
 
+    @NotNull(message = "Email should not be empty")
     private String email;
 
+    @NotNull(message= "password should not be empty")
+    @Size(min= 5, message = "Password should be at least 5 characters")
     private String password;
 
 //    private Long age = 0L;
