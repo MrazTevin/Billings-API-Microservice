3d0cd970a6d67d87f4550065df3a2995b93ad3d3
MrazTevin <millatevin@gmail.com>
2022-10-24 18:23:52 +0300

    m: get existing email

--- src/main/java/com/microservice/springbootbackend/Service/UserServiceImpl.java
+++ src/main/java/com/microservice/springbootbackend/Service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.microservice.springbootbackend.Service;
 
+import com.microservice.springbootbackend.Exception.EmailExistsException;
 import com.microservice.springbootbackend.Models.User;
 import com.microservice.springbootbackend.Models.UserModel;
 import com.microservice.springbootbackend.Repository.UserRepository;
@@ -13,6 +14,9 @@ public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
     @Override
     public User createUser(UserModel user) {
+        if (userRepository.emailAlreadyExists(user.getEmail())) {
+            throw new EmailExistsException("User is already registered with email: " + user.getEmail());
+        }
         User newUser = new User();
         BeanUtils.copyProperties(user, newUser);
         return userRepository.save(newUser);
